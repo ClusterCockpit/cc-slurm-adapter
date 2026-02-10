03e31fe9eebe872bdaf20b3a047ab1bcf138d9b2
Michael Panzlaff <michael.panzlaff@fau.de>
2026-02-10 16:32:19 +0100

    profiler: add sort report

--- internal/profiler/profiler.go
+++ internal/profiler/profiler.go
@@ -1,13 +1,20 @@
 package profiler
 
 import (
+	"cmp"
 	"fmt"
+	"slices"
 	"time"
 )
 
 type measurement struct {
 	begin    time.Time
 	duration time.Duration
+}
+
+type namedMeasurement struct {
+	name string
+	measurement
 }
 
 var (
@@ -46,15 +53,31 @@ func SectionEnd(name string) {
 }
 
 func Report() string {
-	result := fmt.Sprintf("Total time: %.5f sec", total.duration.Seconds())
+	sectionsSorted := make([]*namedMeasurement, 0)
 
 	timeSections := time.Duration(0)
 
 	for name, section := range sections {
 		timeSections += section.duration
-		result += fmt.Sprintf("\n- %s: %.5f sec", name, section.duration.Seconds())
+
+		sectionsSorted = append(sectionsSorted, &namedMeasurement{
+			name:        name,
+			measurement: *section,
+		})
 	}
 
-	result += fmt.Sprintf("\n- <other>: %.5f sec", (total.duration - timeSections).Seconds())
+	sectionsSorted = append(sectionsSorted, &namedMeasurement{
+		name:        "<other>",
+		measurement: measurement{duration: total.duration - timeSections},
+	})
+
+	slices.SortFunc(sectionsSorted, func(a, b *namedMeasurement) int {
+		return -cmp.Compare(a.duration, b.duration)
+	})
+
+	result := fmt.Sprintf("Total time: %.5f sec", total.duration.Seconds())
+	for _, v := range sectionsSorted {
+		result += fmt.Sprintf("\n- %s: %.5f sec", v.name, v.duration.Seconds())
+	}
 	return result
 }
